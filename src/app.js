3d60b2dc32a0efc4856ab3b95ac168ce1abc1c00
Dmytro Hridin <dmytro.hridin@gmail.com>
2018-05-12 00:32:58 +0300

    add authorize step for routes that requires auth add redirecting on Sign In page add js config file change some show.bind to if.bind

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import {inject} from 'aurelia-dependency-injection';
 import {UserService} from './shared/services/user-service';
+import {SharedState} from './shared/state/shared-state';
+import {Redirect} from 'aurelia-router';
 
 @inject(UserService)
 export class App {
@@ -11,13 +13,14 @@ export class App {
 
   configureRouter(config, router) {
     config.title = 'Conduit';
+    config.addAuthorizeStep(AuthorizeStep);
     config.map([
       {route: ['', 'home'], moduleId: 'components/home/home-component', name: 'home', title: 'Home'},
       {route: ['login'], moduleId: 'components/auth/auth-component', name: 'login', title: 'Sign in'},
       {route: ['register'], moduleId: 'components/auth/auth-component', name:'register', title: 'Sign up'},
-      {route: ['settings'], moduleId: 'components/settings/settings-component', name:'settings', title: 'Settings'},
+      {route: ['settings'], moduleId: 'components/settings/settings-component', name:'settings', title: 'Settings', settings: {auth: true}},
       {route: [':name'], moduleId: 'components/profile/profile-component', name:'profile', title: 'Profile'},
-      {route: ['editor/:slug?'], moduleId: 'components/editor/editor-component', name:'editor', title: 'Editor'},
+      {route: ['editor/:slug?'], moduleId: 'components/editor/editor-component', name:'editor', title: 'Editor', settings: {auth: true}},
       {route: ['article/:slug'], moduleId: 'components/article/article-component', name:'article', title: 'article'}
     ]);
 
@@ -26,5 +29,24 @@ export class App {
 
   attached() {
     this.userService.populate();
+  }
+}
+
+@inject(SharedState)
+export class AuthorizeStep {
+  
+  constructor(state) {
+    this.state = state;
+  }
+
+  run(navigationInstruction, next) {
+    if (navigationInstruction.getAllInstructions().some(i => i.config.settings.auth)) {
+      const isLoggedIn = this.state.isAuthenticated;
+      if (!isLoggedIn) {
+        return next.cancel(new Redirect('login'));
+      }
+    }
+
+    return next();
   }
 }
--- src/resources/elements/article-preview.js
+++ src/resources/elements/article-preview.js
@@ -1,6 +1,30 @@
-import {bindable} from 'aurelia-framework';
+import {inject, bindable} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
+import {SharedState} from '../../shared/state/shared-state';
+import {ArticleService} from '../../shared/services/article-service';
 
+@inject(Router, SharedState, ArticleService)
 export class ArticlePreview {
   @bindable article;
   
+  constructor(router, state, articleService) {
+    this.router = router;
+    this.state = state;
+    this.articleService = articleService;
+  }
+
+  onToggleFavorited() {
+    if (!this.state.isAuthenticated) {
+      this.router.navigateToRoute('login');
+      return;
+    }
+    this.article.favorited = !this.article.favorited;
+    if (this.article.favorited) {
+      this.article.favoritesCount++;
+      this.articleService.favorite(this.slug);
+    } else {
+      this.article.favoritesCount--;
+      this.articleService.unfavorite(this.slug);
+    }
+  }
 }
