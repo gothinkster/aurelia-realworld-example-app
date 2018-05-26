abb662cf7b10c3c561732eb1d37851d235bd80cb
loaded02 <ocaptainmycaptain@web.de>
2018-05-26 11:47:06 +0200

    webpack setup experimental

--- src/resources/index.js
+++ src/resources/index.js
@@ -1,8 +1,10 @@
+import {PLATFORM} from 'aurelia-pal';
+
 export function configure(config) {
   config.globalResources([
-    './value-converters/date',
-    './value-converters/format-html',
-    './value-converters/markdown-html',
-    './value-converters/keys',
+    PLATFORM.moduleName('./value-converters/date'),
+    PLATFORM.moduleName('./value-converters/format-html'),
+    PLATFORM.moduleName('./value-converters/markdown-html'),
+    PLATFORM.moduleName('./value-converters/keys'),
   ]);
 }
